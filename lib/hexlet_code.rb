13c9603675eee8e3e4990af2f729e7f98c2860ac
YuraMaksimov <yura.maksimov.04@mail.ru>
2025-01-24 18:46:53 +0300

    Класс Tag, генерирующий html-тэги

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,18 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+  
+  class Tag
+
+    def self.build(tag, **options, &block)
+
+      join_options = options.map { |key, value| "#{key}=\"#{value}\""}.join(' ')
+      tag_options = "#{[tag, join_options].join(' ')}".rstrip
+      result = block_given? ? "<#{tag_options}>#{block.call}</#{tag}>" : "<#{tag_options}>"
+      result
+
+    end
+
+  end
+
 end
