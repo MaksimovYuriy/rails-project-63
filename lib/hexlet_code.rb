ecedb1d3c2b6a0cfb1f3cbfb9ef5fbdf34b37c3e
YuraMaksimov <yura.maksimov.04@mail.ru>
2025-01-25 17:51:31 +0300

    Step 4

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,18 +4,28 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
+
+  User = Struct.new(:name, :job, keyword_init: true)
   
-  class Tag
-
+  module Tag
     def self.build(tag, **options, &block)
-
       join_options = options.map { |key, value| "#{key}=\"#{value}\""}.join(' ')
       tag_options = "#{[tag, join_options].join(' ')}".rstrip
       result = block_given? ? "<#{tag_options}>#{block.call}</#{tag}>" : "<#{tag_options}>"
       result
-
     end
+  end
 
+  def self.form_for(user, **options, &block)
+    action_method_opts = "method=\"post\""
+    if options[:url].nil?
+      action_method_opts = "action=\"#\" #{action_method_opts}"
+    else
+      action_method_opts = "action=\"#{options[:url]}\" #{action_method_opts}"
+      options.delete(:url)
+    end
+    join_options = options.map { |key, value| "#{key}=\"#{value}\""}.join(' ')
+    join_options.empty? ? "<form #{action_method_opts}></form>" : "<form #{action_method_opts} #{join_options}></form>"
   end
 
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -1,13 +1,40 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require_relative '../lib/hexlet_code'
+
+require 'minitest/autorun'
+require 'minitest/power_assert'
 
 class TestHexletCode < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::HexletCode::VERSION
+  
+  def test_form_without_options()
+    user = HexletCode::User.new(name: 'Yura')
+
+    result_string = HexletCode.form_for(user) do |f|
+    end
+
+    expected_string = "<form action=\"#\" method=\"post\"></form>"
+    assert_equal(result_string, expected_string)
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_form_with_class()
+    user = HexletCode::User.new(name: 'Yura')
+
+    result_string = HexletCode.form_for(user, class: 'hexlet-form') do |f|
+    end
+
+    expected_string = "<form action=\"#\" method=\"post\" class=\"hexlet-form\"></form>"
+    assert_equal(result_string, expected_string)
+  end
+
+  def test_form_with_class_and_url()
+    user = HexletCode::User.new(name: 'Yura')
+
+    result_string = HexletCode.form_for(user, class: 'hexlet-form', url: '/profile') do |f|
+    end
+    
+    expected_string = "<form action=\"/profile\" method=\"post\" class=\"hexlet-form\"></form>"
+    assert_equal(result_string, expected_string)
   end
 end
