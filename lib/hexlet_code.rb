470f1dd94829d67bdaaf3ca96a8e711eecba4709
YuraMaksimov <yura.maksimov.04@mail.ru>
2025-01-26 17:42:02 +0300

    Поправки от линтера

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
-require_relative "hexlet_code/version"
+require_relative 'hexlet_code/version'
 
+# Модуль HexletCode позволяет создавать html-формы
 module HexletCode
   class Error < StandardError; end
-  
+
+  # Модуль HexletCode::Tag позволяет создавать простые тэги с указанием атрибутов
   module Tag
     def self.build(tag, **options, &block)
-      join_options = options.map { |key, value| "#{key}=\"#{value}\""}.join(' ')
-      tag_options = "#{[tag, join_options].join(' ')}".rstrip
-      result = block_given? ? "<#{tag_options}>#{block.call}</#{tag}>" : "<#{tag_options}>"
-      result
+      join_options = options.map { |key, value| "#{key}=\"#{value}\"" }.join(' ')
+      tag_options = [tag, join_options].join(' ').to_s.rstrip
+      block_given? ? "<#{tag_options}>#{block.call}</#{tag}>" : "<#{tag_options}>"
     end
   end
 
@@ -19,43 +20,52 @@ module HexletCode
   end
 
   def self.form_for(user, **options, &block)
-
-    action_method_opts = "method=\"post\""
+    action_method_opts = 'method="post"'
     if options[:url].nil?
       action_method_opts = "action=\"#\" #{action_method_opts}"
     else
       action_method_opts = "action=\"#{options[:url]}\" #{action_method_opts}"
       options.delete(:url)
     end
-    join_options = options.map { |key, value| "#{key}=\"#{value}\""}.join(' ')
+    join_options = options.map { |key, value| "#{key}=\"#{value}\"" }.join(' ')
     result = join_options.empty? ? "<form #{action_method_opts}>" : "<form #{action_method_opts} #{join_options}>"
-    
     input_builder = InputBuilder.new(user)
     "#{result}#{block.call(input_builder) if block_given?}</form>"
   end
 
+  # Класс InputBuilder позволяет генерировать input-тэги с указанными атрибутами
   class InputBuilder
-
     def initialize(user)
       @user = user
-      @fields = ""
+      @fields = ''
     end
 
     def input(name, **options)
       value = @user.public_send(name)
+      label = Tag.build('label', for: name) { name.capitalize }
 
       field_html = if options[:as] == :text
-        cols = options.fetch(:cols, 20)
-        rows = options.fetch(:rows, 40)
-        "<textarea name=\"#{name}\" cols=\"#{cols}\" rows=\"#{rows}\">#{value}</textarea>"
-      else
-        join_options = options.map { |key, value| "#{key}=\"#{value}\""}.join(' ')
-        "<input name=\"#{name}\" type=\"text\" value=\"#{value}\" #{join_options}>".gsub(/\s+>/, '>')
-      end
+                     cols = options.fetch(:cols, 20)
+                     rows = options.fetch(:rows, 40)
+                     textarea_tag = Tag.build('textarea', name: name, cols: cols, rows: rows) { value }
+                     "#{label}#{textarea_tag}"
+                   else
+                     input_tag = Tag.build('input', name: name, type: 'text', value: value)
+                     join_options = options.map { |key, value| "#{key}=\"#{value}\"" }.join(' ')
+                     input_tag = input_tag.sub('>', " #{join_options}>")
+                     "#{label}#{input_tag}".gsub(/\s+>/, '>')
+                   end
 
       @fields = "#{@fields}#{field_html}"
     end
 
+    def submit(value = nil)
+      submit_field = if value.nil?
+                       '<input type="submit" value="Save">'
+                     else
+                       "<input type=\"submit\" value=\"#{value}\">"
+                     end
+      @fields = "#{@fields}#{submit_field}"
+    end
   end
 end
-
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -1,46 +1,45 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 require_relative '../lib/hexlet_code'
 
 require 'minitest/autorun'
 require 'minitest/power_assert'
 
 class TestHexletCode < Minitest::Test
-
   User = Struct.new(:name, :job, :gender, keyword_init: true)
-  
-  def test_form_without_options()
+
+  def test_form_without_options
     user = User.new(name: 'Yura')
 
     result_string = HexletCode.form_for(user) do |f|
     end
 
-    expected_string = "<form action=\"#\" method=\"post\"></form>"
+    expected_string = '<form action="#" method="post"></form>'
     assert_equal(result_string, expected_string)
   end
 
-  def test_form_with_class()
+  def test_form_with_class
     user = User.new(name: 'Yura')
 
     result_string = HexletCode.form_for(user, class: 'hexlet-form') do |f|
     end
 
-    expected_string = "<form action=\"#\" method=\"post\" class=\"hexlet-form\"></form>"
+    expected_string = '<form action="#" method="post" class="hexlet-form"></form>'
     assert_equal(result_string, expected_string)
   end
 
-  def test_form_with_class_and_url()
+  def test_form_with_class_and_url
     user = User.new(name: 'Yura')
 
     result_string = HexletCode.form_for(user, class: 'hexlet-form', url: '/profile') do |f|
     end
 
-    expected_string = "<form action=\"/profile\" method=\"post\" class=\"hexlet-form\"></form>"
+    expected_string = '<form action="/profile" method="post" class="hexlet-form"></form>'
     assert_equal(result_string, expected_string)
   end
 
-  def test_form_with_input1()
+  def test_form_with_input1
     user = User.new(name: 'rob', job: 'hexlet', gender: 'm')
 
     result_string = HexletCode.form_for(user) do |f|
@@ -48,11 +47,11 @@ class TestHexletCode < Minitest::Test
       f.input :job, as: :text
     end
 
-    expected_string = "<form action=\"#\" method=\"post\"><input name=\"name\" type=\"text\" value=\"rob\"><textarea name=\"job\" cols=\"20\" rows=\"40\">hexlet</textarea></form>"
+    expected_string = '<form action="#" method="post"><label for="name">Name</label><input name="name" type="text" value="rob"><label for="job">Job</label><textarea name="job" cols="20" rows="40">hexlet</textarea></form>'
     assert_equal(result_string, expected_string)
   end
 
-  def test_form_with_input2()
+  def test_form_with_input2
     user = User.new name: 'rob', job: 'hexlet', gender: 'm'
 
     result_string = HexletCode.form_for user, url: '#' do |f|
@@ -60,31 +59,45 @@ class TestHexletCode < Minitest::Test
       f.input :job
     end
 
-    expected_string = "<form action=\"#\" method=\"post\"><input name=\"name\" type=\"text\" value=\"rob\" class=\"user-input\"><input name=\"job\" type=\"text\" value=\"hexlet\"></form>"
+    expected_string = '<form action="#" method="post"><label for="name">Name</label><input name="name" type="text" value="rob" class="user-input"><label for="job">Job</label><input name="job" type="text" value="hexlet"></form>'
 
     assert_equal(result_string, expected_string)
   end
 
-  def test_form_with_input3()
+  def test_form_with_input3
     user = User.new name: 'rob', job: 'hexlet', gender: 'm'
 
     result_string = HexletCode.form_for user do |f|
       f.input :job, as: :text
     end
 
-    expected_string = "<form action=\"#\" method=\"post\"><textarea name=\"job\" cols=\"20\" rows=\"40\">hexlet</textarea></form>"
+    expected_string = '<form action="#" method="post"><label for="job">Job</label><textarea name="job" cols="20" rows="40">hexlet</textarea></form>'
 
     assert_equal(result_string, expected_string)
   end
 
-  def test_form_with_input4()
+  def test_form_with_input4
     user = User.new name: 'rob', job: 'hexlet', gender: 'm'
 
     result_string = HexletCode.form_for user, url: '#' do |f|
       f.input :job, as: :text, rows: 50, cols: 50
     end
 
-    expected_string = "<form action=\"#\" method=\"post\"><textarea name=\"job\" cols=\"50\" rows=\"50\">hexlet</textarea></form>"
+    expected_string = '<form action="#" method="post"><label for="job">Job</label><textarea name="job" cols="50" rows="50">hexlet</textarea></form>'
+
+    assert_equal(result_string, expected_string)
+  end
+
+  def test_form_with_input_and_submit
+    user = User.new job: 'hexlet'
+
+    result_string = HexletCode.form_for user do |f|
+      f.input :name
+      f.input :job
+      f.submit
+    end
+
+    expected_string = '<form action="#" method="post"><label for="name">Name</label><input name="name" type="text" value=""><label for="job">Job</label><input name="job" type="text" value="hexlet"><input type="submit" value="Save"></form>'
 
     assert_equal(result_string, expected_string)
   end
