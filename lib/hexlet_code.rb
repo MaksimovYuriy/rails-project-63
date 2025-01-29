cb779095cc924e67e884fb63ab767c9fafb8ad54
YuraMaksimov <yura.maksimov.04@mail.ru>
2025-01-29 17:25:04 +0300

    Построение form через Tag.build

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -16,20 +16,10 @@ module HexletCode
   end
 
   def self.form_for(entity, **options, &block)
-    action_method_opts = if options[:method].nil?
-                           'method="post"'
-                         else
-                           "method=\"#{options[:method]}\""
-                         end
-    if options[:url].nil?
-      action_method_opts = "action=\"#\" #{action_method_opts}"
-    else
-      action_method_opts = "action=\"#{options[:url]}\" #{action_method_opts}"
-      options.delete(:url)
-    end
-    join_options = options.map { |key, value| "#{key}=\"#{value}\"" }.join(' ')
-    result = join_options.empty? ? "<form #{action_method_opts}>" : "<form #{action_method_opts} #{join_options}>"
+    options[:action] = options.delete(:url) || '#'
+    options[:method] ||= 'post'
+    sort_hash = { action: options.delete(:action), method: options.delete(:method) }.merge(options)
     input_builder = InputBuilder.new(entity)
-    "#{result}#{block.call(input_builder) if block_given?}</form>"
+    Tag.build('form', **sort_hash) { block.call(input_builder) if block_given? }
   end
 end
