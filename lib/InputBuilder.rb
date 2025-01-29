99cd6b32b3ee3a999249f4a78a250366cefe8cbf
YuraMaksimov <yura.maksimov.04@mail.ru>
2025-01-29 16:41:37 +0300

    Удаление класса Error и перенос InputBuilder в отдельный файл

---
+++ lib/InputBuilder.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'hexlet_code'
+# Класс InputBuilder позволяет генерировать input-тэги с указанными атрибутами
+class InputBuilder
+  def initialize(user)
+    @user = user
+    @fields = ''
+  end
+
+  def input(name, **options)
+    value = @user.public_send(name)
+    label = HexletCode::Tag.build('label', for: name) { name.capitalize }
+    field_html = if options[:as] == :text
+                   cols = options.fetch(:cols, 20)
+                   rows = options.fetch(:rows, 40)
+                   textarea_tag = HexletCode::Tag.build('textarea', name: name, cols: cols, rows: rows) { value }
+                   "#{label}#{textarea_tag}"
+                 else
+                   join_options = options.map { |key, value| "#{key}=\"#{value}\"" }.join(' ')
+                   input_tag = HexletCode::Tag.build('input', name: name, type: 'text', value: value).sub('>',
+                                                                                                          " #{join_options}>")
+                   "#{label}#{input_tag}".gsub(/\s+>/, '>')
+                 end
+    @fields = "#{@fields}#{field_html}"
+  end
+
+  def submit(value = nil)
+    submit_field = if value.nil?
+                     HexletCode::Tag.build('input', type: 'submit', value: 'Save')
+                   else
+                     HexletCode::Tag.build('input', type: 'submit', value: value.to_s)
+                   end
+    @fields = "#{@fields}#{submit_field}"
+  end
+end
--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,10 +2,10 @@
 
 require_relative 'hexlet_code/version'
 
+require_relative 'InputBuilder'
+
 # Модуль HexletCode позволяет создавать html-формы
 module HexletCode
-  class Error < StandardError; end
-
   # Модуль HexletCode::Tag позволяет создавать простые тэги с указанием атрибутов
   module Tag
     def self.build(tag, **options, &block)
@@ -35,39 +35,5 @@ module HexletCode
     result = join_options.empty? ? "<form #{action_method_opts}>" : "<form #{action_method_opts} #{join_options}>"
     input_builder = InputBuilder.new(user)
     "#{result}#{block.call(input_builder) if block_given?}</form>"
-  end
-
-  # Класс InputBuilder позволяет генерировать input-тэги с указанными атрибутами
-  class InputBuilder
-    def initialize(user)
-      @user = user
-      @fields = ''
-    end
-
-    def input(name, **options)
-      value = @user.public_send(name)
-      label = Tag.build('label', for: name) { name.capitalize }
-      field_html = if options[:as] == :text
-                     cols = options.fetch(:cols, 20)
-                     rows = options.fetch(:rows, 40)
-                     textarea_tag = Tag.build('textarea', name: name, cols: cols, rows: rows) { value }
-                     "#{label}#{textarea_tag}"
-                   else
-                     join_options = options.map { |key, value| "#{key}=\"#{value}\"" }.join(' ')
-                     input_tag = Tag.build('input', name: name, type: 'text', value: value).sub('>',
-                                                                                                " #{join_options}>")
-                     "#{label}#{input_tag}".gsub(/\s+>/, '>')
-                   end
-      @fields = "#{@fields}#{field_html}"
-    end
-
-    def submit(value = nil)
-      submit_field = if value.nil?
-                       Tag.build('input', type: 'submit', value: 'Save')
-                     else
-                       Tag.build('input', type: 'submit', value: value.to_s)
-                     end
-      @fields = "#{@fields}#{submit_field}"
-    end
   end
 end
